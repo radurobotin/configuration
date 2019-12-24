3e134b04326b9b6cd893f3ff720fdebb173e14e1
Radu Robotin <radu.robotin@cloudr-solutions.com>
2019-12-24 12:10:25 -0500

    Update one-script-to-rule-them-all.sh

--- leanda/cloud-formation/one-script-to-rule-them-all.sh
+++ leanda/cloud-formation/one-script-to-rule-them-all.sh
@@ -20,14 +20,14 @@ ecs-cli compose --project-name imaging --file docker-compose.imaging.yml --ecs-p
 
 #turning off all services
 
-ecs-cli compose --project-name eventstore --file docker-compose.eventstore.yml --ecs-params ecs-param.1.yml service down --cluster LEANDA  --aws-profile arqisoft
+ecs-cli compose --project-name eventstore --file docker-compose.eventstore.yml --ecs-params ecs-param.1.yml service down --cluster LEANDA --delete-namespace --aws-profile arqisoft
 ecs-cli compose --project-name blob --file docker-compose.blob.yml --ecs-params ecs-param.1.yml service down  --cluster LEANDA   --aws-profile arqisoft
 ecs-cli compose --project-name elastic --file docker-compose.elastic.yml --ecs-params ecs-param.1.yml service down  --cluster LEANDA  --aws-profile arqisoft
 ecs-cli compose --project-name mongo --file docker-compose.mongo.yml --ecs-params ecs-param.1.yml service down --cluster LEANDA --aws-profile arqisoft
 ecs-cli compose --project-name redis --file docker-compose.redis.yml --ecs-params ecs-param.1.yml service down --cluster LEANDA   --aws-profile arqisoft
 ecs-cli compose --project-name rabbitmq --file docker-compose.rabbit.yml --ecs-params ecs-param.1.yml service down  --cluster LEANDA  --aws-profile arqisoft
 
-ecs-cli compose --project-name core-persistence --file docker-compose.core.yml --ecs-params ecs-param.core.yml service down --cluster LEANDA  --aws-profile arqisoft
+ecs-cli compose --project-name core-persistence --file docker-compose.core-persistence..yml --ecs-params ecs-param.core.yml service down --cluster LEANDA  --aws-profile arqisoft
 ecs-cli compose --project-name sagahost --file docker-compose.sagahost.yml --ecs-params ecs-param.core.yml service down --cluster LEANDA  --aws-profile arqisoft
 ecs-cli compose --project-name core-backend --file docker-compose.core-backend.yml --ecs-params ecs-param.core.yml service down --cluster LEANDA  --aws-profile arqisoft
 ecs-cli compose --project-name core-frontend --file docker-compose.core-frontend.yml --ecs-params ecs-param.core.yml service down --cluster LEANDA  --aws-profile arqisoft
