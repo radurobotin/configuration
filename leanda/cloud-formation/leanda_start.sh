056beb57340c50836ff75b39d5af42e86da5b0d6
Rick Zakharov <rick.zakharov@gmail.com>
2020-01-12 23:30:52 -0500

    * Minor formatting changes * Enabled all services

--- leanda/cloud-formation/leanda_start.sh
+++ leanda/cloud-formation/leanda_start.sh
@@ -29,7 +29,10 @@ exit_on_error() {
     fi
 }
 
-echo "AWS-CLI profile selected:  ${p}"
+DATE=`TZ=America/New_York date "+%Y-%m-%d %H:%M"`
+
+echo "AWS-CLI profile selected:  ${p}. Start date-time: ${DATE}"
+
 echo "Starting Cloud Formation Stack deployment ... This might take 15 minutes or more..."
 aws cloudformation deploy --stack-name LeandaLabWiz --template-file cf-backend.yml --capabilities CAPABILITY_IAM --profile=${p} --region=us-east-1 || exit $?
 
@@ -54,40 +57,40 @@ sed "s/I___SUBNET_1___I/$SUBNET1/g;s/I___SUBNET_2___I/$SUBNET2/g;s/I___FARGATE_S
 
 echo "Starting infrastructure services..."
 
-ecs-cli compose --project-name elastic --file docker-compose.elastic.yml --ecs-params ecs-param.elastic.gen.yml service up --launch-type EC2 --create-log-groups  --cluster LEANDA --private-dns-namespace leanda  --vpc=${VPC} --enable-service-discovery  --aws-profile=${p} ;
-ecs-cli compose --project-name mongo --file docker-compose.mongo.yml --ecs-params ecs-param.backend.gen.yml service up --launch-type EC2 --create-log-groups  --cluster LEANDA --private-dns-namespace leanda  --vpc=${VPC} --enable-service-discovery  --aws-profile=${p} ;
-ecs-cli compose --project-name rabbitmq --file docker-compose.rabbit.yml --ecs-params ecs-param.backend.gen.yml service up --launch-type EC2 --create-log-groups  --cluster LEANDA --private-dns-namespace leanda  --vpc=${VPC} --enable-service-discovery  --aws-profile=${p} ;
-ecs-cli compose --project-name redis --file docker-compose.redis.yml --ecs-params ecs-param.backend.gen.yml service up --launch-type EC2 --create-log-groups  --cluster LEANDA --private-dns-namespace leanda  --vpc=${VPC} --enable-service-discovery  --aws-profile=${p} ;
-ecs-cli compose --project-name eventstore --file docker-compose.eventstore.yml --ecs-params ecs-param.backend.gen.yml service up --launch-type EC2 --create-log-groups  --cluster LEANDA --private-dns-namespace leanda  --vpc=${VPC} --enable-service-discovery  --aws-profile=${p} ;
+ecs-cli compose --project-name elastic --file docker-compose.elastic.yml --ecs-params ecs-param.elastic.gen.yml service up --launch-type EC2 --create-log-groups --cluster LEANDA --private-dns-namespace leanda --vpc=${VPC} --enable-service-discover --aws-profile=${p} ;
+ecs-cli compose --project-name mongo --file docker-compose.mongo.yml --ecs-params ecs-param.backend.gen.yml service up --launch-type EC2 --create-log-groups --cluster LEANDA --private-dns-namespace leanda --vpc=${VPC} --enable-service-discover --aws-profile=${p} ;
+ecs-cli compose --project-name rabbitmq --file docker-compose.rabbit.yml --ecs-params ecs-param.backend.gen.yml service up --launch-type EC2 --create-log-groups --cluster LEANDA --private-dns-namespace leanda --vpc=${VPC} --enable-service-discover --aws-profile=${p} ;
+ecs-cli compose --project-name redis --file docker-compose.redis.yml --ecs-params ecs-param.backend.gen.yml service up --launch-type EC2 --create-log-groups --cluster LEANDA --private-dns-namespace leanda --vpc=${VPC} --enable-service-discover --aws-profile=${p} ;
+ecs-cli compose --project-name eventstore --file docker-compose.eventstore.yml --ecs-params ecs-param.backend.gen.yml service up --launch-type EC2 --create-log-groups --cluster LEANDA --private-dns-namespace leanda --vpc=${VPC} --enable-service-discover --aws-profile=${p} ;
 
 echo "Starting system core services..."
 
-ecs-cli compose --project-name blob-storage-web-api --file docker-compose.blob-storage-web-api.yml --ecs-params ecs-param.backend.gen.yml service up --launch-type EC2 --create-log-groups  --cluster LEANDA --private-dns-namespace leanda  --vpc=${VPC} --enable-service-discovery  --aws-profile=${p} ;
-ecs-cli compose --project-name core-persistence --file docker-compose.core-persistence.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups  --cluster LEANDA --private-dns-namespace leanda  --vpc=${VPC} --enable-service-discovery  --aws-profile=${p};
-ecs-cli compose --project-name core-sagahost --file docker-compose.core-sagahost.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups  --cluster LEANDA --private-dns-namespace leanda  --vpc=${VPC} --enable-service-discovery  --aws-profile=${p};
-ecs-cli compose --project-name core-backend --file docker-compose.core-backend.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups  --cluster LEANDA --private-dns-namespace leanda  --vpc=${VPC} --enable-service-discovery  --aws-profile=${p};
-ecs-cli compose --project-name core-frontend --file docker-compose.core-frontend.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups  --cluster LEANDA --private-dns-namespace leanda  --vpc=${VPC} --enable-service-discovery  --aws-profile=${p};
-ecs-cli compose --project-name core-web-api --file docker-compose.core-web-api.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups  --cluster LEANDA --private-dns-namespace leanda  --vpc=${VPC} --enable-service-discovery  --aws-profile=${p};
+ecs-cli compose --project-name blob-storage-web-api --file docker-compose.blob-storage-web-api.yml --ecs-params ecs-param.backend.gen.yml service up --launch-type EC2 --create-log-groups --cluster LEANDA --private-dns-namespace leanda --vpc=${VPC} --enable-service-discovery --aws-profile=${p} ;
+ecs-cli compose --project-name core-persistence --file docker-compose.core-persistence.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups --cluster LEANDA --private-dns-namespace leanda --vpc=${VPC} --enable-service-discovery --aws-profile=${p};
+ecs-cli compose --project-name core-sagahost --file docker-compose.core-sagahost.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups --cluster LEANDA --private-dns-namespace leanda --vpc=${VPC} --enable-service-discovery --aws-profile=${p};
+ecs-cli compose --project-name core-backend --file docker-compose.core-backend.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups --cluster LEANDA --private-dns-namespace leanda --vpc=${VPC} --enable-service-discovery --aws-profile=${p};
+ecs-cli compose --project-name core-frontend --file docker-compose.core-frontend.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups --cluster LEANDA --private-dns-namespace leanda --vpc=${VPC} --enable-service-discovery --aws-profile=${p};
+ecs-cli compose --project-name core-web-api --file docker-compose.core-web-api.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups --cluster LEANDA --private-dns-namespace leanda --vpc=${VPC} --enable-service-discovery --aws-profile=${p};
 
 echo "Starting system secondary services..."
 
-# ecs-cli compose --project-name imaging --file docker-compose.imaging.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups  --cluster LEANDA --private-dns-namespace leanda  --vpc=${VPC} --enable-service-discovery  --aws-profile=${p};
-# ecs-cli compose --project-name indexing --file docker-compose.indexing.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups  --cluster LEANDA --private-dns-namespace leanda  --vpc=${VPC} --enable-service-discovery  --aws-profile=${p};
-# ecs-cli compose --project-name office-processor --file docker-compose.office-processor.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups  --cluster LEANDA --private-dns-namespace leanda  --vpc=${VPC} --enable-service-discovery  --aws-profile=${p};
+ecs-cli compose --project-name imaging --file docker-compose.imaging.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups --cluster LEANDA --private-dns-namespace leanda --vpc=${VPC} --enable-service-discover --aws-profile=${p};
+ecs-cli compose --project-name indexing --file docker-compose.indexing.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups --cluster LEANDA --private-dns-namespace leanda --vpc=${VPC} --enable-service-discover --aws-profile=${p};
+ecs-cli compose --project-name office-processor --file docker-compose.office-processor.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups --cluster LEANDA --private-dns-namespace leanda --vpc=${VPC} --enable-service-discovery --aws-profile=${p};
 
 echo "Starting other services..."
 
-# ecs-cli compose --project-name metadata-processing --file docker-compose.metadata-processing.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups  --cluster LEANDA --private-dns-namespace leanda  --vpc=${VPC} --enable-service-discovery  --aws-profile=${p};
-# ecs-cli compose --project-name chemical-file-parser --file docker-compose.chemical-file-parser.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups  --cluster LEANDA --private-dns-namespace leanda  --vpc=${VPC} --enable-service-discovery  --aws-profile=${p};
-# ecs-cli compose --project-name chemical-properties --file docker-compose.chemical-properties.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups  --cluster LEANDA --private-dns-namespace leanda  --vpc=${VPC} --enable-service-discovery  --aws-profile=${p};
-# ecs-cli compose --project-name chemical-export --file docker-compose.chemical-export.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups  --cluster LEANDA --private-dns-namespace leanda  --vpc=${VPC} --enable-service-discovery  --aws-profile=${p};
-# ecs-cli compose --project-name reaction-parser --file docker-compose.reaction-parser.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups  --cluster LEANDA --private-dns-namespace leanda  --vpc=${VPC} --enable-service-discovery  --aws-profile=${p};
-# ecs-cli compose --project-name crystal-parser --file docker-compose.crystal-parser.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups  --cluster LEANDA --private-dns-namespace leanda  --vpc=${VPC} --enable-service-discovery  --aws-profile=${p};
-# ecs-cli compose --project-name spectra-parser --file docker-compose.spectra-parser.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups  --cluster LEANDA --private-dns-namespace leanda  --vpc=${VPC} --enable-service-discovery  --aws-profile=${p};
-# ecs-cli compose --project-name web-importer --file docker-compose.web-importer.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups  --cluster LEANDA --private-dns-namespace leanda  --vpc=${VPC} --enable-service-discovery  --aws-profile=${p};
-# ecs-cli compose --project-name microscopy-metadata --file docker-compose.microscopy-metadata.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups  --cluster LEANDA --private-dns-namespace leanda  --vpc=${VPC} --enable-service-discovery  --aws-profile=${p};
-# ecs-cli compose --project-name categories --file docker-compose.categories.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups  --cluster LEANDA --private-dns-namespace leanda  --vpc=${VPC} --enable-service-discovery  --aws-profile=${p};
+ecs-cli compose --project-name metadata-processing --file docker-compose.metadata-processing.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups --cluster LEANDA --private-dns-namespace leanda --vpc=${VPC} --enable-service-discovery --aws-profile=${p};
+ecs-cli compose --project-name chemical-file-parser --file docker-compose.chemical-file-parser.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups --cluster LEANDA --private-dns-namespace leanda --vpc=${VPC} --enable-service-discovery --aws-profile=${p};
+ecs-cli compose --project-name chemical-properties --file docker-compose.chemical-properties.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups --cluster LEANDA --private-dns-namespace leanda --vpc=${VPC} --enable-service-discovery --aws-profile=${p};
+ecs-cli compose --project-name chemical-export --file docker-compose.chemical-export.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups --cluster LEANDA --private-dns-namespace leanda --vpc=${VPC} --enable-service-discovery --aws-profile=${p};
+ecs-cli compose --project-name reaction-parser --file docker-compose.reaction-parser.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups --cluster LEANDA --private-dns-namespace leanda --vpc=${VPC} --enable-service-discovery --aws-profile=${p};
+ecs-cli compose --project-name crystal-parser --file docker-compose.crystal-parser.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups --cluster LEANDA --private-dns-namespace leanda --vpc=${VPC} --enable-service-discovery --aws-profile=${p};
+ecs-cli compose --project-name spectra-parser --file docker-compose.spectra-parser.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups --cluster LEANDA --private-dns-namespace leanda --vpc=${VPC} --enable-service-discovery --aws-profile=${p};
+ecs-cli compose --project-name web-importer --file docker-compose.web-importer.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups --cluster LEANDA --private-dns-namespace leanda --vpc=${VPC} --enable-service-discovery --aws-profile=${p};
+ecs-cli compose --project-name microscopy-metadata --file docker-compose.microscopy-metadata.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups --cluster LEANDA --private-dns-namespace leanda --vpc=${VPC} --enable-service-discovery --aws-profile=${p};
+ecs-cli compose --project-name categories --file docker-compose.categories.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups --cluster LEANDA --private-dns-namespace leanda --vpc=${VPC} --enable-service-discovery --aws-profile=${p};
 
 echo "Starting proxy service..."
 
-ecs-cli compose --project-name nginx --file docker-compose.nginx.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups  --cluster LEANDA --private-dns-namespace leanda  --vpc=${VPC} --enable-service-discovery  --aws-profile=${p};
+ecs-cli compose --project-name nginx --file docker-compose.nginx.yml --ecs-params ecs-param.core.gen.yml service up --launch-type FARGATE --create-log-groups --cluster LEANDA --private-dns-namespace leanda --vpc=${VPC} --enable-service-discover --aws-profile=${p};
--- leanda/cloud-formation/leanda_stop.sh
+++ leanda/cloud-formation/leanda_stop.sh
@@ -29,50 +29,50 @@ exit_on_error() {
     fi
 }
 
-echo "AWS-CLI profile selected:  ${p}"
+DATE=`TZ=America/New_York date "+%Y-%m-%d %H:%M"`
 
-echo "Removing running containers from the stack..."
+echo "AWS-CLI profile selected: ${p}. Start date-time: ${DATE}"
 
 echo "Removing proxy service..."
 
-ecs-cli compose --project-name nginx --file docker-compose.nginx.yml  service down --cluster LEANDA  --aws-profile=${p};
+ecs-cli compose --project-name nginx --file docker-compose.nginx.yml service down --cluster LEANDA --aws-profile=${p};
 
 echo "Removing other services..."
 
-# ecs-cli compose --project-name metadata-processing --file docker-compose.metadata-processing.yml  service down --cluster LEANDA  --aws-profile=${p};
-# ecs-cli compose --project-name chemical-file-parser --file docker-compose.chemical-file-parser.yml   service down --cluster LEANDA  --aws-profile=${p};
-# ecs-cli compose --project-name chemical-properties --file docker-compose.chemical-properties.yml   service down --cluster LEANDA  --aws-profile=${p};
-# ecs-cli compose --project-name chemical-export --file docker-compose.chemical-export.yml  service down --cluster LEANDA  --aws-profile=${p};
-# ecs-cli compose --project-name reaction-parser --file docker-compose.reaction-parser.yml  service down --cluster LEANDA  --aws-profile=${p};
-# ecs-cli compose --project-name crystal-parser --file docker-compose.crystal-parser.yml  service down --cluster LEANDA  --aws-profile=${p};
-# ecs-cli compose --project-name spectra-parser --file docker-compose.spectra-parser.yml  service down --cluster LEANDA  --aws-profile=${p};
-# ecs-cli compose --project-name web-importer --file docker-compose.web-importer.yml  service down --cluster LEANDA  --aws-profile=${p};
-# ecs-cli compose --project-name categories --file docker-compose.categories.yml  service down --cluster LEANDA  --aws-profile=${p};
-# ecs-cli compose --project-name microscopy-metadata --file docker-compose.microscopy-metadata.yml service down --cluster LEANDA  --aws-profile=${p};
+ecs-cli compose --project-name metadata-processing --file docker-compose.metadata-processing.yml service down --cluster LEANDA --aws-profile=${p};
+ecs-cli compose --project-name chemical-file-parser --file docker-compose.chemical-file-parser.yml service down --cluster LEANDA --aws-profile=${p};
+ecs-cli compose --project-name chemical-properties --file docker-compose.chemical-properties.yml service down --cluster LEANDA --aws-profile=${p};
+ecs-cli compose --project-name chemical-export --file docker-compose.chemical-export.yml service down --cluster LEANDA --aws-profile=${p};
+ecs-cli compose --project-name reaction-parser --file docker-compose.reaction-parser.yml service down --cluster LEANDA --aws-profile=${p};
+ecs-cli compose --project-name crystal-parser --file docker-compose.crystal-parser.yml service down --cluster LEANDA --aws-profile=${p};
+ecs-cli compose --project-name spectra-parser --file docker-compose.spectra-parser.yml service down --cluster LEANDA --aws-profile=${p};
+ecs-cli compose --project-name web-importer --file docker-compose.web-importer.yml service down --cluster LEANDA --aws-profile=${p};
+ecs-cli compose --project-name categories --file docker-compose.categories.yml service down --cluster LEANDA --aws-profile=${p};
+ecs-cli compose --project-name microscopy-metadata --file docker-compose.microscopy-metadata.yml service down --cluster LEANDA --aws-profile=${p};
 
 echo "Removing system secondary services..."
 
-# ecs-cli compose --project-name office-processor --file docker-compose.office-processor.yml  service down --cluster LEANDA  --aws-profile=${p};
-# ecs-cli compose --project-name imaging --file docker-compose.imaging.yml  service down --cluster LEANDA  --aws-profile=${p};
-# ecs-cli compose --project-name indexing --file docker-compose.indexing.yml   service down --cluster LEANDA  --aws-profile=${p};
+ecs-cli compose --project-name office-processor --file docker-compose.office-processor.yml service down --cluster LEANDA --aws-profile=${p};
+ecs-cli compose --project-name imaging --file docker-compose.imaging.yml service down --cluster LEANDA --aws-profile=${p};
+ecs-cli compose --project-name indexing --file docker-compose.indexing.yml service down --cluster LEANDA --aws-profile=${p};
 
 echo "Removing system core services..."
 
-ecs-cli compose --project-name core-web-api --file docker-compose.core-web-api.yml  service down --cluster LEANDA  --aws-profile=${p};
-ecs-cli compose --project-name core-frontend --file docker-compose.core-frontend.yml  service down --cluster LEANDA  --aws-profile=${p};
-ecs-cli compose --project-name core-backend --file docker-compose.core-backend.yml service down --cluster LEANDA  --aws-profile=${p};
-ecs-cli compose --project-name core-sagahost --file docker-compose.core-sagahost.yml   service down --cluster LEANDA  --aws-profile=${p};
-ecs-cli compose --project-name core-persistence --file docker-compose.core-persistence.yml  service down --cluster LEANDA  --aws-profile=${p};
-ecs-cli compose --project-name blob-storage-web-api --file docker-compose.blob-storage-web-api.yml  service down --cluster LEANDA  --aws-profile=${p};
+ecs-cli compose --project-name core-web-api --file docker-compose.core-web-api.yml service down --cluster LEANDA --aws-profile=${p};
+ecs-cli compose --project-name core-frontend --file docker-compose.core-frontend.yml service down --cluster LEANDA --aws-profile=${p};
+ecs-cli compose --project-name core-backend --file docker-compose.core-backend.yml service down --cluster LEANDA --aws-profile=${p};
+ecs-cli compose --project-name core-sagahost --file docker-compose.core-sagahost.yml service down --cluster LEANDA --aws-profile=${p};
+ecs-cli compose --project-name core-persistence --file docker-compose.core-persistence.yml service down --cluster LEANDA --aws-profile=${p};
+ecs-cli compose --project-name blob-storage-web-api --file docker-compose.blob-storage-web-api.yml service down --cluster LEANDA --aws-profile=${p};
 
 echo "Removing infrastructure services..."
 
-ecs-cli compose --project-name eventstore --file docker-compose.eventstore.yml  service down --cluster LEANDA  --aws-profile=${p};
-ecs-cli compose --project-name redis --file docker-compose.redis.yml  service down --cluster LEANDA  --aws-profile=${p};
-ecs-cli compose --project-name rabbitmq --file docker-compose.rabbit.yml   service down --cluster LEANDA  --aws-profile=${p};
-ecs-cli compose --project-name mongo --file docker-compose.mongo.yml  service down --cluster LEANDA  --aws-profile=${p};
-ecs-cli compose --project-name elastic --file docker-compose.elastic.yml  service down --cluster LEANDA --delete-namespace --aws-profile=${p};
+ecs-cli compose --project-name eventstore --file docker-compose.eventstore.yml service down --cluster LEANDA --aws-profile=${p};
+ecs-cli compose --project-name redis --file docker-compose.redis.yml service down --cluster LEANDA --aws-profile=${p};
+ecs-cli compose --project-name rabbitmq --file docker-compose.rabbit.yml service down --cluster LEANDA --aws-profile=${p};
+ecs-cli compose --project-name mongo --file docker-compose.mongo.yml service down --cluster LEANDA --aws-profile=${p};
+ecs-cli compose --project-name elastic --file docker-compose.elastic.yml service down --cluster LEANDA --delete-namespace --aws-profile=${p};
 
 echo "Removing main CloudFormation stack ..."
 
-aws cloudformation delete-stack --stack-name LeandaLabWiz   --profile=${p}  || exit $?
+aws cloudformation delete-stack --stack-name LeandaLabWiz  --profile=${p} || exit $?
