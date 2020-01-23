38000ad15e706df122d9801854cd1c5408734e0c
Radu Robotin <radu.robotin@cloudr-solutions.com>
2020-01-22 21:03:28 -0500

    Update leanda_stop.sh

--- leanda/cloud-formation/leanda_stop.sh
+++ leanda/cloud-formation/leanda_stop.sh
@@ -33,44 +33,47 @@ echo "Private DNS Domain name will be: ${PRIVATE_DNS}"
 echo "Removing running containers from the stack..."
 
 
-ecs-cli compose --project-name nginx --file docker-compose.nginx.yml service down --cluster ${s} --aws-profile=${p};
+ecs-cli compose --project-name nginx --file docker-compose.nginx.yml service down --cluster ${s} --aws-profile=${p} || true
 
 # echo "Removing other services..."
 
-ecs-cli compose --project-name metadata-processing --file docker-compose.metadata-processing.yml service down --cluster ${s} --aws-profile=${p};
-ecs-cli compose --project-name chemical-file-parser --file docker-compose.chemical-file-parser.yml service down --cluster ${s} --aws-profile=${p};
-ecs-cli compose --project-name chemical-properties --file docker-compose.chemical-properties.yml service down --cluster ${s} --aws-profile=${p};
-ecs-cli compose --project-name chemical-export --file docker-compose.chemical-export.yml service down --cluster ${s} --aws-profile=${p};
-ecs-cli compose --project-name reaction-parser --file docker-compose.reaction-parser.yml service down --cluster ${s} --aws-profile=${p};
-ecs-cli compose --project-name crystal-parser --file docker-compose.crystal-parser.yml service down --cluster ${s} --aws-profile=${p};
-ecs-cli compose --project-name spectra-parser --file docker-compose.spectra-parser.yml service down --cluster ${s} --aws-profile=${p};
-ecs-cli compose --project-name web-importer --file docker-compose.web-importer.yml service down --cluster ${s} --aws-profile=${p};
-ecs-cli compose --project-name categories --file docker-compose.categories.yml service down --cluster ${s} --aws-profile=${p};
-ecs-cli compose --project-name microscopy-metadata --file docker-compose.microscopy-metadata.yml service down --cluster ${s} --aws-profile=${p};
+ecs-cli compose --project-name metadata-processing --file docker-compose.metadata-processing.yml service down --cluster ${s} --aws-profile=${p} || :
+ecs-cli compose --project-name chemical-file-parser --file docker-compose.chemical-file-parser.yml service down --cluster ${s} --aws-profile=${p} || :
+ecs-cli compose --project-name chemical-properties --file docker-compose.chemical-properties.yml service down --cluster ${s} --aws-profile=${p} || :
+ecs-cli compose --project-name chemical-export --file docker-compose.chemical-export.yml service down --cluster ${s} --aws-profile=${p} || :
+ecs-cli compose --project-name reaction-parser --file docker-compose.reaction-parser.yml service down --cluster ${s} --aws-profile=${p} || :
+ecs-cli compose --project-name crystal-parser --file docker-compose.crystal-parser.yml service down --cluster ${s} --aws-profile=${p} || :
+ecs-cli compose --project-name spectra-parser --file docker-compose.spectra-parser.yml service down --cluster ${s} --aws-profile=${p} || :
+ecs-cli compose --project-name web-importer --file docker-compose.web-importer.yml service down --cluster ${s} --aws-profile=${p} || :
+ecs-cli compose --project-name categories --file docker-compose.categories.yml service down --cluster ${s} --aws-profile=${p} || :
+ecs-cli compose --project-name microscopy-metadata --file docker-compose.microscopy-metadata.yml service down --cluster ${s} --aws-profile=${p}  || :
 
 echo "Removing system secondary services..."
 
-ecs-cli compose --project-name office-processor --file docker-compose.office-processor.yml service down --cluster ${s} --aws-profile=${p};
-ecs-cli compose --project-name imaging --file docker-compose.imaging.yml service down --cluster ${s} --aws-profile=${p};
-ecs-cli compose --project-name indexing --file docker-compose.indexing.yml service down --cluster ${s} --aws-profile=${p};
+ecs-cli compose --project-name office-processor --file docker-compose.office-processor.yml service down --cluster ${s} --aws-profile=${p} || :
+ecs-cli compose --project-name imaging --file docker-compose.imaging.yml service down --cluster ${s} --aws-profile=${p} || :
+ecs-cli compose --project-name indexing --file docker-compose.indexing.yml service down --cluster ${s} --aws-profile=${p} || :
 
 echo "Removing system core services..."
 
-ecs-cli compose --project-name core-web-api --file docker-compose.core-web-api.yml service down --cluster ${s} --aws-profile=${p};
-ecs-cli compose --project-name core-frontend --file docker-compose.core-frontend.yml service down --cluster ${s} --aws-profile=${p};
-ecs-cli compose --project-name core-backend --file docker-compose.core-backend.yml service down --cluster ${s} --aws-profile=${p};
-ecs-cli compose --project-name core-sagahost --file docker-compose.core-sagahost.yml service down --cluster ${s} --aws-profile=${p};
-ecs-cli compose --project-name core-persistence --file docker-compose.core-persistence.yml service down --cluster ${s} --aws-profile=${p};
-ecs-cli compose --project-name blob-storage-web-api --file docker-compose.blob-storage-web-api.yml service down --cluster ${s} --aws-profile=${p};
+ecs-cli compose --project-name core-web-api --file docker-compose.core-web-api.yml service down --cluster ${s} --aws-profile=${p} || :
+ecs-cli compose --project-name core-frontend --file docker-compose.core-frontend.yml service down --cluster ${s} --aws-profile=${p} || :
+ecs-cli compose --project-name core-backend --file docker-compose.core-backend.yml service down --cluster ${s} --aws-profile=${p} || :
+ecs-cli compose --project-name core-sagahost --file docker-compose.core-sagahost.yml service down --cluster ${s} --aws-profile=${p} || :
+ecs-cli compose --project-name core-persistence --file docker-compose.core-persistence.yml service down --cluster ${s} --aws-profile=${p} || :
+ecs-cli compose --project-name blob-storage-web-api --file docker-compose.blob-storage-web-api.yml service down --cluster ${s} --aws-profile=${p} || :
 
 echo "Removing infrastructure services..."
 
-ecs-cli compose --project-name eventstore --file docker-compose.eventstore.yml service down --cluster ${s} --aws-profile=${p};
-ecs-cli compose --project-name redis --file docker-compose.redis.yml service down --cluster ${s} --aws-profile=${p};
-ecs-cli compose --project-name rabbitmq --file docker-compose.rabbit.yml service down --cluster ${s} --aws-profile=${p};
-ecs-cli compose --project-name mongo --file docker-compose.mongo.yml service down --cluster ${s} --aws-profile=${p};
-ecs-cli compose --project-name elastic --file docker-compose.elastic.yml service down --cluster ${s} --delete-namespace --aws-profile=${p};
+ecs-cli compose --project-name eventstore --file docker-compose.eventstore.yml service down --cluster ${s} --aws-profile=${p} || :
+ecs-cli compose --project-name redis --file docker-compose.redis.yml service down --cluster ${s} --aws-profile=${p} || :
+ecs-cli compose --project-name rabbitmq --file docker-compose.rabbit.yml service down --cluster ${s} --aws-profile=${p} || :
+ecs-cli compose --project-name mongo --file docker-compose.mongo.yml service down --cluster ${s} --aws-profile=${p} || :
+ecs-cli compose --project-name elastic --file docker-compose.elastic.yml service down --cluster ${s} --delete-namespace --aws-profile=${p} || :
 
 echo "Removing main CloudFormation stack ..."
+
+FR_STACK_NAME="${s}-frontend"
+aws cloudformation delete-stack --stack-name ${FR_STACK_NAME} --profile=${p} || :
 
 aws cloudformation delete-stack --stack-name ${s}  --profile=${p} || exit $?
